6a4d98cd198abbc26f4050407ae696f70ede16cc
Naziullah Shawn <naziullahshawn135@gmail.com>
2021-04-29 12:14:29 +0600

    almost finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { createContext, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import AddDoctor from "./components/AddDoctor/AddDoctor";
-import AllPatients from "./components/AllPatients/AllPatients.js/AllPatients";
-import Appointment from "./components/Appointment/Appointment/Appointment";
-import PrivateRoute from './components/Login/PrivateRoute/PrivateRoute';
-import Dashboard from "./components/Dashboard/Dashboard/Dashboard";
-import Home from "./components/Home/Home/Home";
-import Login from "./components/Login/Login/Login";
+
+import AllPatients from "../components/AllPatients/AllPatients.js/AllPatients";
+import Appointment from "../components/Appointment/Appointment/Appointment";
+import PrivateRoute from "../components/Login/PrivateRoute/PrivateRoute";
+import Dashboard from "../components/Dashboard/Dashboard/Dashboard";
+import Home from "../components/Home/Home/Home";
+import Login from "../components/Login/Login/Login";
+import AddDoctor from "../components/AddDoctor/AddDoctor";
 
 export const UserContext = createContext();
 
@@ -15,28 +16,28 @@ function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
   return (
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/appointment">
-          <Appointment></Appointment>
-        </Route>
-        <PrivateRoute path="/dashboard">
-          <Dashboard></Dashboard>
-        </PrivateRoute>
-        <Route to="/login">
-          <Login></Login>
-        </Route>
-        <Route path="/dashboard/allPatients">
-          <AllPatients></AllPatients>
-        </Route>
-        <Route path="/addDoctor">
-          <AddDoctor></AddDoctor>
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/appointment">
+            <Appointment></Appointment>
+          </Route>
+          <PrivateRoute path="/dashboard">
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <Route to="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/dashboard/allPatients">
+            <AllPatients></AllPatients>
+          </Route>
+          <Route path="/addDoctor">
+            <AddDoctor></AddDoctor>
+          </Route>
+        </Switch>
+      </Router>
     </UserContext.Provider>
   );
 }
